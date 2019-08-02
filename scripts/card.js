54413eb20c3aef1b7e3f4269af46b04ed39cbb56
mrayhanhakim <mrayhanhakim2@gmail.com>
2019-08-02 14:59:29 +0700

    Add fallback function for select/ban map if page saved on device

--- scripts/card.js
+++ scripts/card.js
@@ -1,3 +1,18 @@
+/* 
+ * Fallback for saved result 
+ * for select/ban event listener
+*/
+const select = document.getElementsByClassName("selectMap");
+const ban = document.getElementsByClassName("banMap");
+
+for (let i = 0; i < select.length; i++) {
+  select[i].addEventListener("click", e => selectMap(e));
+  select[i].addEventListener("contextmenu", e => selectMap(e));
+  ban[i].addEventListener("click", e => banMap(e));
+  ban[i].addEventListener("contextmenu", e => banMap(e));
+}
+/*--------------------------------------------------------------*/
+
 /* Result Page Functions */
 function selectMap(event) {
   event.preventDefault();
@@ -5,10 +20,8 @@ function selectMap(event) {
   unselectMap(select);
 
   const teamSelected = event.button === 0 ? 1 : 0;
-  
-  select.parentNode.classList.add(
-    `${teamColor[teamSelected]}`
-  );
+
+  select.parentNode.classList.add(`${teamColor[teamSelected]}`);
 }
 
 function banMap(event) {
@@ -17,9 +30,9 @@ function banMap(event) {
   banned.classList.add("banned");
 }
 
-function unselectMap(unselect) {
-  unselect.classList.remove("banned");
-  unselect.parentNode.removeAttribute("class");
-  unselect.parentNode.classList.add("mapContainer");
+function unselectMap(event) {
+  event.classList.remove("banned");
+  event.parentNode.removeAttribute("class");
+  event.parentNode.classList.add("mapContainer");
 }
 /* -------------------------------------------------------------------------------- */
